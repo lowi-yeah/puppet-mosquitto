b5d96746c7e8dbe6fa0a4bd0e4e565ddf45f44b9
initial commit <git@lowi.org>
2016-04-25 17:51:16 +0200

    adding custom yum repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,17 +2,37 @@
 #
 class mosquitto::install inherits mosquitto {
 
-  package { 'mosquitto':
-    ensure => $package_ensure,
-    name   => $package_name,
-  }
-
+ 
   file { $system_log_dir:
     ensure => directory,
     owner  => $mosquitto::user,
     group  => $mosquitto::group,
     mode   => '0755',
   }
+
+  yumrepo { "home_oojah_mqtt":
+    ensure => true,
+    baseurl => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/home:oojah:mqtt.repo",
+    descr => "CentOS Mosquitto repository",
+    enabled => 1,
+    gpgcheck => 1,
+    gpgkey => "http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6//repodata/repomd.xml.key"
+  }
+
+  package { 'mosquitto':
+    ensure => $package_ensure,
+    name   => $package_name,
+    require => Yumrepo["home_oojah_mqtt"]
+  }
+
+# http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/home:oojah:mqtt.repo
+# [home_oojah_mqtt]
+# name=mqtt (CentOS_CentOS-6)
+# type=rpm-md
+# baseurl=http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6/
+# gpgcheck=1
+# gpgkey=http://download.opensuse.org/repositories/home:/oojah:/mqtt/CentOS_CentOS-6//repodata/repomd.xml.key
+# enabled=1
 
   if $limits_manage == true {
     limits::fragment {
